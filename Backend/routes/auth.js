8e9baaa0b7e9879e4e622b4cadfbe392c31e2ff6
Mahesh More <maheshmore667@gmail.com>
2023-07-22 14:12:54 +0530

    validation layer is added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,18 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express?.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post('/createUser', (req, res)=>{
-    try{
-        console.log(req.body);
-        const user = new User(req.body);
+//validation layer added 
+router.post(
+  "/createUser",
+  body("email", "please enter a valid email").isEmail(),
+  body("name", "please enter a valid name").isLength(3),
+  body("password", "please enter a valid password").isLength(3),
+  (req, res) => {
+    try {
+      console.log(req.body);
+      const user = new User(req.body);
+      const result = validationResult(req);
+      if (result.isEmpty()) {
         user.save();
-        res.send({status : 200, description : "SuccessFul"});
-    }catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    } 
-    
-});
+        res.send({ status: 200, description: "SuccessFul" });
+      } else {
+        res.send({ description: "invalid payload", errors: result.array() });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+);
 
 module.exports = router;
