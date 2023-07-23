0321e068684b051db9d3aa6a48f00c1a0978e16b
Mahesh More <maheshmore667@gmail.com>
2023-07-23 20:53:35 +0530

    login user API made

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 //validation layer added
+
+//API : create user API
 router.post(
   "/createUser",
   body("email", "please enter a valid email").isEmail(),
@@ -13,7 +15,6 @@ router.post(
   body("password", "please enter a valid password").isLength(3),
   async (req, res) => {
     try {
-      console.log(req.body);
       const result = validationResult(req);
       if (result.isEmpty()) {
         //check if the user already exists
@@ -54,5 +55,44 @@ router.post(
     }
   }
 );
+
+
+//API : login user API
+router.post( "/login",
+body("email", "please enter a valid email").isEmail(),
+body("password", "Please enter the password").exists(),
+async (req, res) => {
+  try {
+    const result = validationResult(req);
+    if(result.isEmpty()){
+
+      //finding the user with entered credentials
+      let user = await User.findOne({ email: req?.body?.email });
+
+      //if user exists
+      if(user){
+        //check password 
+        const passwordCompare = await bcrypt?.compare(req.body?.password, user.password);
+        if(passwordCompare) {
+          const data = {
+            user: {
+              id : User?.id
+            }
+          }
+          const authToken = jwt?.sign(data, "Cl@udYNote$");
+          res.status(200).send({message : "Success" , authToken });
+        } else {
+          res.status(400).send({description : "Please enter the valid credentials"});
+        }
+      } else {
+        res.status(400).send({description : "Please enter the valid credentials of valid user"});
+      }
+    } else {
+      res.status(400).send({description : "Please enter the valid credentials"});
+    }
+  } catch(error){
+    res.status(500).send({description : "Internal server error"});
+  }
+});
 
 module.exports = router;
