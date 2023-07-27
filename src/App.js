5e10c7c7ac9d86ecc737ead1a25d1184fa7e3ba8
Mahesh More <maheshmore667@gmail.com>
2023-07-27 13:24:21 +0530

    basic application is setup using routing and it's components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navlist from "./components/Navlist";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navlist />
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/About">
+          <About/>
+        </Route>
+      </Router>
+    </>
   );
 }
 
