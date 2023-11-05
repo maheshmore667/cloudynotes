ae3cced92943ca0cc01e6af7c09bd9099ef3824d
Mahesh More <maheshmore667@gmail.com>
2023-11-05 21:45:47 +0530

    minor code sanitization

--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -8,8 +8,8 @@ const Notesitem = (props) => {
         <div className="card-body ">
           <div className="d-flex align-items-center">
             <h5 className="card-title mx-2">{notes.title}</h5> 
-            <i class="fa-solid fa-trash mx-2"></i>
-            <i class="fa-regular fa-pen-to-square mx-2"></i>
+            <i className="fa-solid fa-trash mx-2"></i>
+            <i className="fa-regular fa-pen-to-square mx-2"></i>
           </div>
          
           <p className="card-text">
