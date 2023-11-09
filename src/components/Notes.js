9dc2ac979aeffc84c4cdef939d60c1df726d415c
Mahesh More <maheshmore667@gmail.com>
2023-11-09 19:05:09 +0530

    Edit note basic modal is made

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,23 +1,111 @@
-import React , { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from "react";
 import noteContext from "../context/notes/noteContext";
-import Notesitem from './Notesitem';
-import AddNotes from './AddNotes';
+import Notesitem from "./Notesitem";
+import AddNotes from "./AddNotes";
 
 const Notes = () => {
- const { notes, fetchNote } = useContext(noteContext);
- useEffect(()=>{
-  fetchNote(); // eslint-disable-next-line
- },[])
+  const { notes, fetchNote } = useContext(noteContext);
+  useEffect(() => {
+    fetchNote(); // eslint-disable-next-line
+  }, []);
+  
   return (
     <div className="container my-3">
-      <AddNotes/>
-      <div className="row">
-          { notes?.map((note) => {
-             return <Notesitem key={note._id}  notes={note}/>
-          })}
-        </div>
-    </div>
-  )
-}
+      <AddNotes />
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
 
-export default Notes
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Edit Your Note
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form className="my-3">
+                <div className="form-group my-3">
+                  <label className="mb-2" htmlFor="title">
+                    Note Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    name="title"
+                    aria-describedby="Note Title"
+                    placeholder="Enter title of the note"
+                  ></input>
+                </div>
+                <div className="form-group my-3">
+                  <label className="mb-2" htmlFor="description">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="description"
+                    name="description"
+                    aria-describedby="Note Description"
+                    placeholder="description"
+                  ></input>
+                </div>
+                <div className="form-group my-3">
+                  <label className="mb-2" htmlFor="tag">
+                    Tag
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="tag"
+                    name="tag"
+                    aria-describedby="Note Tag"
+                    placeholder="tag"
+                  ></input>
+                </div>
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center">
+        {notes?.map((note) => {
+          return <Notesitem key={note._id} notes={note} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Notes;
--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -2,21 +2,25 @@ import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
 
 const Notesitem = (props) => {
-  const { notes } = props
-  const {deleteNote} = useContext(noteContext);
+  const { notes } = props;
+  const { deleteNote } = useContext(noteContext);
   return (
     <>
+     
       <div className="card col-md-3 mx-1 my-2">
         <div className="card-body ">
           <div className="d-flex align-items-center">
-            <h5 className="card-title mx-2">{notes.title}</h5> 
-            <i className="fa-solid fa-trash mx-2" onClick={()=>{deleteNote(notes._id)}}></i>
+            <h5 className="card-title mx-2">{notes.title}</h5>
+            <i
+              className="fa-solid fa-trash mx-2"
+              onClick={() => {
+                deleteNote(notes._id);
+              }}
+            ></i>
             <i className="fa-regular fa-pen-to-square mx-2"></i>
           </div>
-         
-          <p className="card-text">
-            {notes.description}
-          </p>
+
+          <p className="card-text">{notes.description}</p>
         </div>
       </div>
     </>
