54b26a9054b45cf077d216c6b93c468d83915489
Mahesh More <maheshmore667@gmail.com>
2023-11-10 21:06:09 +0530

    login component template with form is ready

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      This is login
-    </div>
-  )
-}
+    <div
+      className="container-fluid d-flex justify-content-center align-items-center"
+      style={{ backgroundColor: "#E3DDDB", height:"100vh"}}
+    >
+        
+      <div className="col-md-6" style={{ backgroundColor: "white" }}>
+      
+        <form style={{height:"50vh"}} className="d-flex flex-column justify-content-center align-items-center">
+        <h3 className="mb-2">CloudyNotes</h3>
 
-export default Login
+          <div className="form-group col-md-10 mb-4">
+            <label htmlFor="Email" >Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="Email"
+              name="Email"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="form-group col-md-10 mb-5">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
