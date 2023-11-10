ca8fd5686e45beb8ae56dd660b6c51d50034382d
Mahesh More <maheshmore667@gmail.com>
2023-11-10 21:41:29 +0530

    Client side code done for login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [creds, setCreds] = useState({ email: "", password: "", name: "" })
+  const handleInput= (e) =>{
+    setCreds({...creds, [e.target.name] : e.target.value});
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(creds);
+  }
+
   return (
     <div
       className="container-fluid d-flex justify-content-center align-items-center"
-      style={{ backgroundColor: "#E3DDDB", height:"100vh"}}
+      style={{ backgroundColor: "#E3DDDB", height: "100vh" }}
     >
-        
-      <div className="col-md-6" style={{ backgroundColor: "white" }}>
-      
-        <form style={{height:"50vh"}} className="d-flex flex-column justify-content-center align-items-center">
-        <h3 className="mb-2">CloudyNotes</h3>
+      <div
+        className="col-md-6"
+        style={{ backgroundColor: "white", borderRadius: "10px" }}
+      >
+        <form
+          style={{ height: "60vh" }}
+          className="d-flex flex-column justify-content-center align-items-center"
+          onSubmit={handleSubmit}
+        >
+          <h3 >CloudyNotes</h3>
 
           <div className="form-group col-md-10 mb-4">
-            <label htmlFor="Email" >Email address</label>
+            <b>
+              <label htmlFor="email" className="mb-1">
+                Email address
+              </label>
+            </b>
             <input
               type="email"
               className="form-control"
-              id="Email"
-              name="Email"
+              id="email"
+              name="email"
               aria-describedby="emailHelp"
               placeholder="Enter email"
+              onChange={handleInput}
             />
           </div>
           <div className="form-group col-md-10 mb-5">
-            <label htmlFor="password">Password</label>
+            <b>
+              <label htmlFor="password" className="mb-1">
+                Password
+              </label>
+            </b>
             <input
               type="password"
               className="form-control"
               id="password"
               name="password"
               placeholder="Password"
+              onChange={handleInput}
+            />
+          </div>
+          <div className="form-group col-md-10 mb-5">
+            <b>
+              <label htmlFor="name" className="mb-1">
+                Name
+              </label>
+            </b>
+            <input
+              type="name"
+              className="form-control"
+              id="name"
+              name="name"
+              placeholder="name"
+              onChange={handleInput}
             />
           </div>
           <button type="submit" className="btn btn-primary">
